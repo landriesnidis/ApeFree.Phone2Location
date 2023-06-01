67a5f2fe1cf821ac6cc777be90b2934d8fd72ed6
Guijie Lee <landriesnidis@yeah.net>
2023-06-02 01:33:05 +0800

    增加构造中国手机号归属地数据库扩展方法

---
+++ ApeFree.Phone2Location.CN/PhoneDatabaseFactoryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using ApeFree.Phone2Location.CN;
+
+namespace ApeFree.Phone2Location
+{
+    public static class PhoneDatabaseFactoryExtensions
+    {
+        /// <summary>
+        /// 构造中国手机号归属地数据库
+        /// </summary>
+        /// <param name="_"></param>
+        /// <returns></returns>
+        public static ChinaPhoneDatabase CreateChinaPhoneDatabase(this PhoneDatabaseFactory _)
+        {
+            return new ChinaPhoneDatabase();
+        }
+    }
+}
