0a34772cb0d55a14af84b64f65c450288268a192
Guijie Lee <landriesnidis@yeah.net>
2023-06-02 01:33:37 +0800

    更新Demo

---
+++ ApeFree.Phone2Location.Demo/TestForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ApeFree.Phone2Location.Demo
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labResult = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPhone = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.labResult);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(719, 311);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "查询结果";
+            // 
+            // labResult
+            // 
+            this.labResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labResult.Font = new System.Drawing.Font("微软雅黑", 15F);
+            this.labResult.Location = new System.Drawing.Point(3, 17);
+            this.labResult.Name = "labResult";
+            this.labResult.Padding = new System.Windows.Forms.Padding(20);
+            this.labResult.Size = new System.Drawing.Size(713, 291);
+            this.labResult.TabIndex = 0;
+            this.labResult.Text = "查询结果";
+            this.labResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15F);
+            this.label2.Location = new System.Drawing.Point(12, 356);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "请输入手机号码：";
+            // 
+            // tbPhone
+            // 
+            this.tbPhone.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPhone.Font = new System.Drawing.Font("宋体", 15F);
+            this.tbPhone.Location = new System.Drawing.Point(187, 352);
+            this.tbPhone.Name = "tbPhone";
+            this.tbPhone.Size = new System.Drawing.Size(544, 30);
+            this.tbPhone.TabIndex = 2;
+            this.tbPhone.TextChanged += new System.EventHandler(this.tbPhone_TextChanged);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTest.Location = new System.Drawing.Point(571, 397);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(160, 40);
+            this.btnTest.TabIndex = 3;
+            this.btnTest.Text = "循环测试";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 449);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.tbPhone);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "TestForm";
+            this.Text = "TestForm";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPhone;
+        private System.Windows.Forms.Button btnTest;
+    }
+}
+
---
+++ ApeFree.Phone2Location.Demo/TestForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ApeFree.Phone2Location.Demo
+{
+    public partial class TestForm : Form
+    {
+        private const int TotalCycles = 1000000;
+
+        private PhoneDatabase db;
+
+        public TestForm()
+        {
+            InitializeComponent();
+
+            // 创建数据库
+            db = PhoneDatabase.Factory.CreateChinaPhoneDatabase();
+        }
+
+        private void tbPhone_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // 通过手机号码获取归属地信息
+                var result = db.GetLocationByPhone(tbPhone.Text);
+
+                if (result == null)
+                {
+                    labResult.Text = "未查询到结果";
+                }
+                else
+                {
+                    // 打印显示
+                    labResult.Text = $"国家：{result.Country}\r\n省份：{result.Province}\r\n城市：{result.City}\r\n运营商：{result.TelecomOperator}\r\n区号：{result.AreaCode}\r\n邮编：{result.PostalCode}";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // 打印错误信息
+                labResult.Text = ex.Message;
+            }
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbPhone.Text))
+            {
+                tbPhone.Text = "18066668888";
+            }
+
+            string phone = tbPhone.Text;
+
+            int i = TotalCycles;
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start(); //计时开始
+
+            while (i-- > 0)
+            {
+                _ = db.GetLocationByPhone(phone);
+            }
+
+            sw.Stop(); //计时结束
+
+            MessageBox.Show($"调用 {TotalCycles} 次。\r\n总耗时 {sw.ElapsedMilliseconds} ms", "测试结束");
+        }
+    }
+}
