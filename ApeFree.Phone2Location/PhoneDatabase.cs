9c06f5f818f594d7de55cd1ccf49c2de9e6d8203
Guijie Lee <landriesnidis@yeah.net>
2023-06-02 01:31:07 +0800

    手机号码归属地数据库核心库

---
+++ ApeFree.Phone2Location/PhoneDatabase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ApeFree.Phone2Location
+{
+    /// <summary>
+    /// 手机号码归属地数据库
+    /// </summary>
+    public abstract class PhoneDatabase : IDisposable
+    {
+        private static readonly Lazy<PhoneDatabaseFactory> lazyInstance = new Lazy<PhoneDatabaseFactory>(() => new PhoneDatabaseFactory());
+
+        /// <summary>
+        /// 获取手机号码归属地构造工厂
+        /// </summary>
+        public static PhoneDatabaseFactory Factory
+        {
+            get
+            {
+                return lazyInstance.Value;
+            }
+        }
+
+        /// <summary>
+        /// 数据发布时间
+        /// </summary>
+        public abstract DateTime ReleaseTime { get; }
+
+        /// <summary>
+        /// 通过手机号码获取归属地信息
+        /// </summary>
+        /// <param name="phone">手机号码</param>
+        /// <returns>查询结果</returns>
+        public abstract QueryResult GetLocationByPhone(string phone);
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public abstract void Dispose();
+    }
+
+    /// <summary>
+    /// 手机号码归属地数据库构造工厂
+    /// </summary>
+    public class PhoneDatabaseFactory
+    {
+        internal PhoneDatabaseFactory() { }
+    }
+}
---
+++ ApeFree.Phone2Location/QueryResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApeFree.Phone2Location
+{
+    public abstract class QueryResult
+    {
+        /// <summary>
+        /// 国家名称
+        /// </summary>
+        public virtual string Country { get; }
+
+        /// <summary>
+        /// 省份
+        /// </summary>
+        public virtual string Province { get; }
+
+        /// <summary>
+        /// 城市
+        /// </summary>
+        public virtual string City { get; }
+
+        /// <summary>
+        /// 邮政编码
+        /// </summary>
+        public virtual string PostalCode { get; }
+
+        /// <summary>
+        /// 区号
+        /// </summary>
+        public virtual string AreaCode { get; }
+
+        /// <summary>
+        /// 电信运营商
+        /// </summary>
+        public virtual string TelecomOperator { get; }
+    }
+}
